0e166c029c644939cdc1556cc4f9c74c3c417e71
tomastryggva <32614259+tomastryggva@users.noreply.github.com>
2020-02-26 23:48:23 +0000

    hhh

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const path = require('path');
 const cors = require("cors");
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    next();
-  });
-
-app.use(cors({ origin: "*" }));
-
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/training'));
 
