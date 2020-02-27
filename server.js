c1de812da525d5163090edda0653f549b3fd9487
tomastryggva <32614259+tomastryggva@users.noreply.github.com>
2020-02-27 00:13:01 +0000

    ss

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const cors = require("cors");
 const app = express();
 
 // Serve only the static files form the dist directory
