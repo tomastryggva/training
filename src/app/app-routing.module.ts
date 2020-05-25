c23e2d0fc11559ac1f050e2ee3efb4c9f350e1f0
tomastryggva <32614259+tomastryggva@users.noreply.github.com>
2020-05-25 23:53:06 +0000

    logo fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { NavButtonsComponent } from './nav-buttons/nav-buttons.component';
 import { InfoComponent } from './info/info.component';
-import { FooterComponent } from './footer/footer.component';
 import { PriceComponent } from './price/price.component';
 
 
 const routes: Routes = [
   { path: '', component: HeaderComponent},
   { path: '', component: NavButtonsComponent},
-  { path: '', component: InfoComponent},
-  { path: '', component: PriceComponent},
-  { path: '', component: LoginComponent},
-  { path: '', component: FooterComponent}
+  { path: 'about', component: InfoComponent},
+  { path: '#resume', component: PriceComponent},
+  { path: '#contact', component: LoginComponent}
 ];
 
 @NgModule({
